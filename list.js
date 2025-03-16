f401d4005b992eabc962d4e8c2139b168db6c230
Ruggery <ruggeryalves@hotmail.com>
2025-03-16 11:08:06 +0000

    counter button

---
+++ list.js
@@ -0,0 +1,20 @@
+let subheading = document.getElementsByTagName("h2")[0];
+
+subheading.addEventListener("mouseover", () => { //Anonymoys Funcion
+    subheading.removeAttribute("class");
+})
+
+subheading.addEventListener("mouseout", () => {
+    subheading.setAttribute("class", "green");
+})
+
+let counterButton = document.getElementById("counter-button");
+let count = 0;
+counterButton.addEventListener("click", () => {
+    // console.log(count);
+    // document.getElementById("counter-text").innerText = count++;
+    // console.log(count);
+    let textNode = document.getElementById("counter-text");
+    let currentValue = textNode.innerText;
+    textNode.innerText = ++currentValue;
+})
